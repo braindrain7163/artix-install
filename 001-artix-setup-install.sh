7d2bb0fd4009d63c2212e130df4ce76750244ee1
braindrain7163 <94963752+braindrain7163@users.noreply.github.com>
2024-12-20 21:23:23 +1000

    Create 001-artix-setup-install.sh

---
+++ 001-artix-setup-install.sh
@@ -0,0 +1,88 @@
+
+# Append the tags and their content at the end of the file
+for tag_content in "${tags[@]}"; do
+    IFS=":" read -r tag content <<< "$tag_content"
+    echo -e "\n$tag\n$content" >> "$file"
+done
+
+# Update and populate keyrings
+sudo pacman -Sy gnupg archlinux-keyring artix-keyring --noconfirm
+sudo pacman-key --populate archlinux artix
+
+# Full system upgrade
+sudo pacman -Syuu --noconfirm
+
+#addbuild stuff
+sudo pacman -S curl git base-devel --noconfirm
+
+# Set up yay (AUR Helper)
+mkdir -p ~/source/arch-packages
+
+if ! is_yay_installed; then
+  echo "yay not found, proceeding to build..."
+
+  cd ~/source/arch-packages
+
+  if [ ! -d "yay" ]; then
+    git clone https://aur.archlinux.org/yay.git
+  else
+    cd ~/source/arch-packages/yay
+    git pull || exit 1
+  fi
+
+  cd ~/source/arch-packages/yay
+  makepkg -si --noconfirm
+else
+  echo "yay is already installed, checking for updates..."
+
+  if [ -d "~/source/arch-packages/yay" ]; then
+    cd ~/source/arch-packages/yay
+    git fetch
+    LOCAL=$(git rev-parse HEAD)
+    REMOTE=$(git rev-parse @{u})
+
+    if [ "$LOCAL" != "$REMOTE" ]; then
+      echo "yay source has changed, rebuilding..."
+      git pull
+      makepkg -si --noconfirm
+    else
+      echo "yay is up-to-date, skipping rebuild."
+    fi
+  else
+    echo "/git/yay does not exist. Please check your setup."
+    exit 1
+  fi
+fi
+
+# Install PyYAML
+sudo pacman -S world/python-yaml --noconfirm
+
+#install larbs
+mkdir -p ~/source/arch-packages
+cd ~/source/arch-packages
+curl -LO larbs.xyz/larbs.sh
+echo "to install larbs run: sh larbs.sh"
+
+#fix grub
+sudo pacman -S grub os-prober --noconfirm
+sudo os-prober
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+#setup services
+sudo mkdir /run/runit/service -P
+sudo ln -s /etc/runit/sv/connmand /run/runit/service/    
+sudo ln -s /etc/runit/sv/connmand /etc/runit/runsvdir/default
+# ln -s /etc/runit/sv/NetworkManager /run/runit/service/
+
+sudo ln -s /etc/runit/sv/dhcpcd /run/runit/service/
+sudo ln -s /etc/runit/sv/dhcpcd /etc/runit/runsvdir/default
+
+sudo ln -s /etc/runit/sv/bluetoothd /run/runit/service/
+sudo ln -s /etc/runit/sv/bluetoothd /etc/runit/runsvdir/default
+
+sudo ln -s /etc/runit/sv/cupsd /run/runit/service/
+sudo ln -s /etc/runit/sv/cupsd /etc/runit/runsvdir/default
+
+sudo ln -s /etc/runit/sv/acpid /run/runit/service/
+sudo ln -s /etc/runit/sv/acpid /etc/runit/runsvdir/default
+
