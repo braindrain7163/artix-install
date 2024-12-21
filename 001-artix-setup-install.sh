9a61b013168b48a03bf1d8c89155b653df6a1fba
braindrain7163 <94963752+braindrain7163@users.noreply.github.com>
2024-12-21 17:09:25 +1000

    Update 001-artix-setup-install.sh

--- 001-artix-setup-install.sh
+++ 001-artix-setup-install.sh
@@ -13,7 +13,7 @@ sudo pacman-key --populate archlinux artix
 sudo pacman -Syuu --noconfirm
 
 #addbuild stuff
-sudo pacman -S curl git base-devel --noconfirm
+sudo pacman -S curl git base-devel artix-archlinux-support --noconfirm
 
 if ! is_yay_installed; then
   echo "yay not found, proceeding to build..."
