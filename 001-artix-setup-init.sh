0db1a3c38c511396d5e5a590923124b1932716ee
braindrain7163 <94963752+braindrain7163@users.noreply.github.com>
2024-12-21 13:32:19 +1000

    Update 001-artix-setup-init.sh

--- 001-artix-setup-init.sh
+++ 001-artix-setup-init.sh
@@ -4,14 +4,21 @@
 mkdir -p ~/source/arch-packages
 
 # Update mirrorlists
-sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-arch
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-arch-backup
 sudo curl -o /etc/pacman.d/mirrorlist https://gitea.artixlinux.org/packages/artix-mirrorlist/raw/branch/master/mirrorlist
 
-sudo curl -o ~/source/arch-packages/mirrorlist-arch https://archlinux.org/mirrorlist/?country=AU&protocol=http&protocol=https&ip_version=4
+# Download the Arch mirrorlist to a temporary location
+curl -o ~/source/arch-packages/mirrorlist-arch https://archlinux.org/mirrorlist/?country=AU&protocol=http&protocol=https&ip_version=4
 
-sudo mv /etc/pacman.d/mirrorlist-arch
+# Uncomment servers in the downloaded Arch mirrorlist
+sed -i 's/^#Server/Server/' ~/source/arch-packages/mirrorlist-arch
 
-sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist-arch
+# Move the modified Arch mirrorlist to the appropriate location
+sudo cp ~/source/arch-packages/mirrorlist-arch /etc/pacman.d/mirrorlist-arch
+
+# Set ownership of the mirrorlists to root
+sudo chown root:root /etc/pacman.d/mirrorlist
+sudo chown root:root /etc/pacman.d/mirrorlist-arch
 
 # Define the file and tags
 file="/etc/pacman.conf"
@@ -21,13 +28,19 @@ tags=(
     "[multilib]:Include = /etc/pacman.d/mirrorlist-arch"
 )
 
-if not file.bak
-cp file file.bak
-else
-cl file.bak file
+# Backup the pacman.conf file if not already backed up
+if [[ ! -f "$file.bak" ]]; then
+  sudo cp "$file" "$file.bak"
+elif [[ -f "$file.bak" ]]; then
+  sudo cp "$file.bak" "$file"
+fi
 
-# Append the tags and their content at the end of the file
+# Append the tags and their content at the end of the file if not already present
 for tag_content in "${tags[@]}"; do
     IFS=":" read -r tag content <<< "$tag_content"
-    echo -e "\n$tag\n$content" >> "$file"
+    if ! grep -q "$tag" "$file"; then
+        echo -e "\n$tag\n$content" | sudo tee -a "$file" >/dev/null
+    else
+        echo "$tag already exists in $file"
+    fi
 done
