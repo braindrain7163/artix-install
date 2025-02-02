b556bd5f9777cd39c902cba4e36fb361a61a086a
braindrain7163 <94963752+braindrain7163@users.noreply.github.com>
2025-02-02 17:59:23 +1000

    Update 000-artix-install.sh

--- 000-artix-install.sh
+++ 000-artix-install.sh
@@ -44,7 +44,7 @@ TIMEZONE=${TIMEZONE:-Australia/Brisbane}  # Default to 'Australia/Brisbane'
 echo "Step 1: Install the base system."
 read -p "Do you want to install the base system? (y/n): " CONFIRM
 if [[ "$CONFIRM" =~ ^[Yy]$ ]]; then
-    basestrap $MOUNT_POINT base base-devel runit elogind-runit linux linux-firmware nano intel-ucode git vi man-db neovim sudo networkmanager networkmanager-runit openssh openssh-runit
+    basestrap $MOUNT_POINT base base-devel runit elogind elogind-runit linux linux-firmware nano intel-ucode git vi man-db neovim sudo networkmanager networkmanager-runit openssh openssh-runit
     echo "Base system installed."
 else
     echo "Skipping base system installation."
